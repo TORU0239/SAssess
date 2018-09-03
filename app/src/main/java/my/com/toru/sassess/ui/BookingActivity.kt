9b7e2696104f9c6096a426eb48a1beff6d775d97
Toru Choi <toru.choi@11street.my>
2018-09-03 10:36:27 +0800

    added map to booking activity

--- app/src/main/java/my/com/toru/sassess/ui/BookingActivity.kt
+++ app/src/main/java/my/com/toru/sassess/ui/BookingActivity.kt
@@ -3,11 +3,15 @@ package my.com.toru.sassess.ui
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.util.Log
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
 import my.com.toru.sassess.R
 import my.com.toru.sassess.model.DropOffLocations
 import java.util.*
 
-class BookingActivity : AppCompatActivity() {
+class BookingActivity : AppCompatActivity(), OnMapReadyCallback {
+
     companion object {
         private val TAG = BookingActivity::class.java.simpleName
     }
@@ -28,5 +32,11 @@ class BookingActivity : AppCompatActivity() {
             Log.w(TAG, "drop off latitude:: ${each.location[0]} // drop-off longitude:: ${each.location[1]}")
         }
         Log.w(TAG, "==========================")
+
+
+        val mapFragment = supportFragmentManager.findFragmentById(R.id.booking_map) as SupportMapFragment
+        mapFragment.getMapAsync(this)
     }
+
+    override fun onMapReady(map: GoogleMap?) {}
 }
