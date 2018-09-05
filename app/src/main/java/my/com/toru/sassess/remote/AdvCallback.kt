781a848000f7a85f130be7e3213d527e922f6d3b
Toru Choi <toru.choi@11street.my>
2018-09-05 13:16:12 +0800

    Retry-enabled callback

--- app/src/main/java/my/com/toru/sassess/remote/AdvCallback.kt
+++ app/src/main/java/my/com/toru/sassess/remote/AdvCallback.kt
@@ -7,18 +7,36 @@ import retrofit2.Response
 open class AdvCallback<T>(private val failedCallback:()->Unit,
                           private val successCallback:(Response<T>)->Unit): Callback<T> {
 
+    private val THRESHOLD = 2
+    private var count = 0
+
     override fun onFailure(call: Call<T>, t: Throwable) {
-        failedCallback()
+        retryCall(call)
     }
 
     override fun onResponse(call: Call<T>, response: Response<T>) {
-        when(response.code()){
-            200->{
-                successCallback(response)
+        if(response.isSuccessful){
+            when(response.code()){
+                200->{
+                    successCallback(response)
+                }
+                else->{
+                    failedCallback()
+                }
             }
-            else->{
-                failedCallback()
-            }
+        }
+        else{
+            retryCall(call)
+        }
+    }
+
+    private fun retryCall(call:Call<T>){
+        if(count < THRESHOLD){
+            call.clone().enqueue(this@AdvCallback)
+            count+=1
+        }
+        else{
+            failedCallback()
         }
     }
 }
